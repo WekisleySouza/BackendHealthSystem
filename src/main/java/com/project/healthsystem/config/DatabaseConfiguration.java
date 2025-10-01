1b622f3074a797603ddc39c93afbdd6178b7f3dc
Wekisley Souza <wekisleysouza.a@gmail.com>
2025-10-01 17:14:27 -0300

    env

--- src/main/java/com/project/healthsystem/config/DatabaseConfiguration.java
+++ src/main/java/com/project/healthsystem/config/DatabaseConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -14,33 +16,29 @@ public class DatabaseConfiguration {
     @Value("${spring.datasource.url}")
     private String url;
 
-    @Value("${spring.datasource.username:}")
-    private String username;
-
-    @Value("${spring.datasource.password:}")
-    private String password;
-
-    @Value("${spring.datasource.driverClassName:org.postgresql.Driver}")
-    private String driver;
-
     @Bean
-    public DataSource hikariDataSource() {
+    public DataSource hikariDataSource() throws URISyntaxException {
         HikariConfig config = new HikariConfig();
 
-        String jdbcUrl = url;
-        if (jdbcUrl != null) {
-            if (jdbcUrl.startsWith("postgres://")) {
-                jdbcUrl = jdbcUrl.replace("postgres://", "jdbc:postgresql://");
-            } else if (jdbcUrl.startsWith("postgresql://")) {
-                jdbcUrl = jdbcUrl.replace("postgresql://", "jdbc:postgresql://");
-            }
+        // Se vier algo como: postgres://user:pass@host:5432/db
+        if (url.startsWith("postgres://") || url.startsWith("postgresql://")) {
+            URI uri = new URI(url);
+
+            String userInfo = uri.getUserInfo(); // "user:pass"
+            String username = userInfo.split(":")[0];
+            String password = userInfo.split(":")[1];
+
+            String jdbcUrl = "jdbc:postgresql://" + uri.getHost() + ":" + uri.getPort() + uri.getPath()
+                    + "?sslmode=require";
+
+            config.setJdbcUrl(jdbcUrl);
+            config.setUsername(username);
+            config.setPassword(password);
+        } else {
+            config.setJdbcUrl(url);
         }
 
-        config.setJdbcUrl(jdbcUrl);
-        config.setUsername(username);
-        config.setPassword(password);
-        config.setDriverClassName(driver);
-
+        config.setDriverClassName("org.postgresql.Driver");
         config.setMaximumPoolSize(10);
         config.setMinimumIdle(1);
         config.setPoolName("healthsystem-db-pool");
