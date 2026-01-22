0e470c56888370ac0583b466a8ed920c5e8d311d
Wekisley Souza <wekisleysouza.a@gmail.com>
2026-01-22 10:23:25 -0300

    Atualizar o Permissions.java

--- src/main/java/com/project/healthsystem/controller/common/Permissions.java
+++ src/main/java/com/project/healthsystem/controller/common/Permissions.java
@@ -3,23 +3,23 @@ package com.project.healthsystem.controller.common;
 public final class Permissions {
     public static final String ADMIN_OR_MANAGER =
         "hasAnyRole(" +
-            "T(com.project.healthsystem.model.Roles).ADMIN.name()," +
-            "T(com.project.healthsystem.model.Roles).MANAGER.name()" +
+            "T(com.project.healthsystem.model.Roles).ADMIN.getLabel()," +
+            "T(com.project.healthsystem.model.Roles).MANAGER.getLabel()" +
         ")";
 
     public static final String ADMIN_OR_MANAGER_OR_EMPLOYEE =
         "hasAnyRole(" +
-            "T(com.project.healthsystem.model.Roles).ADMIN.name()," +
-            "T(com.project.healthsystem.model.Roles).MANAGER.name()," +
-            "T(com.project.healthsystem.model.Roles).EMPLOYEE.name()" +
+            "T(com.project.healthsystem.model.Roles).ADMIN.getLabel()," +
+            "T(com.project.healthsystem.model.Roles).MANAGER.getLabel()," +
+            "T(com.project.healthsystem.model.Roles).EMPLOYEE.getLabel()" +
         ")";
 
     public static final String ADMIN_OR_MANAGER_OR_EMPLOYEE_OR_PATIENT =
         "hasAnyRole(" +
-            "T(com.project.healthsystem.model.Roles).ADMIN.name()," +
-            "T(com.project.healthsystem.model.Roles).MANAGER.name()," +
-            "T(com.project.healthsystem.model.Roles).MANAGER.name()," +
-            "T(com.project.healthsystem.model.Roles).PATIENT.name()" +
+            "T(com.project.healthsystem.model.Roles).ADMIN.getLabel()," +
+            "T(com.project.healthsystem.model.Roles).MANAGER.getLabel()," +
+            "T(com.project.healthsystem.model.Roles).MANAGER.getLabel()," +
+            "T(com.project.healthsystem.model.Roles).PATIENT.getLabel()" +
         ")";
 
     public static final String PERMIT_ALL = "permitAll()";
