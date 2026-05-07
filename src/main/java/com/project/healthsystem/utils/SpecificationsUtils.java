2c878313ce836649af400881b5f5d6d1d9ca0585
Wekisley Souza <wekisleysouza.a@gmail.com>
2026-05-07 17:53:43 -0300

    feat(SpecificationsUtils): normalise accepting all simbols.

--- src/main/java/com/project/healthsystem/utils/SpecificationsUtils.java
+++ src/main/java/com/project/healthsystem/utils/SpecificationsUtils.java
@@ -1,8 +1,5 @@
 package com.project.healthsystem.utils;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.Expression;
-
 import java.text.Normalizer;
 
 public class SpecificationsUtils {
@@ -10,7 +7,7 @@ public class SpecificationsUtils {
     public static String normalize(String value) {
         return Normalizer.normalize(value, Normalizer.Form.NFD)
             .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
-            .replaceAll("[^A-Za-z0-9 \\-]", "")
+//            .replaceAll("[^A-Za-z0-9 \\-]", "")
             .replaceAll("\\s+", " ")
             .toUpperCase()
             .trim();
