2c155287ead3838196ece9008620d5bcfd88fdfb
Wekisley Souza <wekisleysouza.a@gmail.com>
2026-05-18 17:53:08 -0300

    refactor(password-recuperation): new model to password recuperation.

--- src/main/java/com/project/healthsystem/service/EmailService.java
+++ src/main/java/com/project/healthsystem/service/EmailService.java
@@ -1,30 +1,54 @@
 package com.project.healthsystem.service;
 
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
 
     @Value("${app.frontend-url}")
     private String frontendUrl;
 
-    public void sendResetPasswordEmail(String to, String token){
-        String link = frontendUrl + "/reset-password?token=" + token;
+    public void sendResetPasswordEmail(
+            String to,
+            String token
+    ) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            String link = frontendUrl + "/reset-password?token=" + token;
 
-        message.setTo(to);
-        message.setSubject("Recuperação de senha");
-        message.setText(
-            "Clique no link para redefinir sua senha:\n" + link
-        );
+            Context context = new Context();
 
-        mailSender.send(message);
+            context.setVariable("resetLink", link);
+
+            String html = templateEngine.process("reset-password", context);
+
+            MimeMessage message = mailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom(new InternetAddress("projEmp25@gmail.com", "Vitalya"));
+
+            helper.setTo(to);
+
+            helper.setSubject("Redefinição de senha");
+
+            helper.setText(html, true);
+
+            mailSender.send(message);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
